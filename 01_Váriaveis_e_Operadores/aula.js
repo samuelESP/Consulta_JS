ab97ae685b68d5e4d2eb2301f1eaa94d9cf7fb73
samuelESP <samuelnegocios1509@gmail.com>
2022-11-17 17:21:54 -0300

    update dir

---
+++ 01_Váriaveis_e_Operadores/aula.js
@@ -0,0 +1,7 @@
+//console.log("conteúdo") --> é usado para visualizar no console minha informação
+//Váriaveis são espaços de memória reservado para algum tipo de dado
+//Váriaveis podem ser 'let', 'var' ou 'const'
+let x = 10;
+var y = 5;
+const b = 1;
+console.log(x, y, b);
