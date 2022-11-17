a20cfbd999354c11108c65ae6f84dec39bfc26aa
samuelESP <samuelnegocios1509@gmail.com>
2022-11-17 17:32:31 -0300

    Update aula.js

--- 01_Váriaveis_e_Operadores/aula.js
+++ 01_Váriaveis_e_Operadores/aula.js
@@ -1,7 +1,12 @@
 //console.log("conteúdo") --> é usado para visualizar no console minha informação
 //Váriaveis são espaços de memória reservado para algum tipo de dado
 //Váriaveis podem ser 'let', 'var' ou 'const'
-let x = 10;
-var y = 5;
-const b = 1;
+let x = 10;//BlockScoping; não faz Hoisting.
+var y = 5; //escopo global; permite Hoisting.
+const b = 2;//BlockScoping; não faz Hoisting; Como se trata de uma constante ela não deve ser alterada.
 console.log(x, y, b);
+
+// "=" é o valor de atribuição.
+//"+" soma; "-" subtração; "/" DIvidir; "*" multiplicar; "%" resto da divisão; "**"potencia.
+let a = 2 * 2;
+console.log(y**b, a);
