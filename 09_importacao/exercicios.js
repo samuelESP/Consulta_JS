835f3e9a144ae93cc502fa17892f7d5393cc8159
samuelESP <samuelnegocios1509@gmail.com>
2022-11-27 23:22:28 -0300

    Create importacoes

---
+++ 09_importacao/exercicios.js
@@ -0,0 +1,24 @@
+const { gets, print} = require("./funcoes-auxiliares");
+
+/*1) Faça um programa que receba a quantidade de números e seus respectivos valores. 
+Imprima o maior e o menor número deste conjunto.
+
+Exemplo:
+    Entrada:
+        4
+        100
+        150
+        90
+        200
+
+    Saída:
+        Maior número: 200
+        Menor número: 90*/
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+
+
+            
---
+++ 09_importacao/main.js
@@ -0,0 +1,10 @@
+const funcoes = require('./funcoes-auxiliares'); //--> Importando, para importar algo alguem tem que exportar esse algo.(module.exports)
+
+console.log(funcoes);
+console.log(funcoes.gets());
+//Ou eu posso fazer assim tambem:
+//const {gets, print} = require('./funcoes-auxiliares');
+//console.log(gets());
+
+
+
